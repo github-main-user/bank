141df607f3a652ce9be27e67ab0fa7e1afd0ee4c
github-main-user <kolya.skripn@mail.ru>
2024-12-19 15:14:45 +0300

    implement get_date function

--- src/widget.py
+++ src/widget.py
@@ -28,3 +28,16 @@ def mask_account_card(card_info: str) -> str:
     mask_function = get_mask_account if len(card_number) > 16 else get_mask_card_number
     masked_number = mask_function(int(card_number))
     return card_type + masked_number
+
+
+def get_date(date_time: str) -> str:
+    """
+    Принимает строку с датой в формате: "2024-03-11T02:26:18.671407":
+    Возвращает строку с датой в формате: "ДД.ММ.ГГГГ" ("11.03.2024")
+    """
+    # Отсечь и разделить дату
+    date = date_time[:10]
+    splitted_date = date.split('-')
+
+    # Развенуть и склеить дату точками
+    return '.'.join(splitted_date[::-1])
