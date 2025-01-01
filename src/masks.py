3ecaeb32a85bb7bd4b02efd5b0a4c21e07d000e6
github-main-user <kolya.skripn@mail.ru>
2025-01-01 19:06:26 +0300

    add exception on wrong number length

--- src/masks.py
+++ src/masks.py
@@ -1,3 +1,7 @@
+class WrongNumberLengthError(Exception):
+    pass
+
+
 def get_mask_card_number(card_number: int) -> str:
     """
     Функция принимает на вход номер карты в виде числа.
@@ -5,6 +9,9 @@ def get_mask_card_number(card_number: int) -> str:
     """
 
     mask = list(str(card_number))  # Преобразовать номер в список символов
+    if len(mask) != 16:
+        raise WrongNumberLengthError("Номер карты должен состоять из 16 цифр.")
+
     mask[6:12] = "*" * 6  # "Скрыть" символы с 6 до 12
 
     # Разбить номер на чанки по 4 символа в каждом
@@ -18,4 +25,8 @@ def get_mask_account(account_number: int) -> str:
     Функция примимает на вход номер счёта в виде числа.
     Возвращает маску формата: **XXXX (X - цифра номера)
     """
+
+    if len(str(account_number)) != 20:
+        raise WrongNumberLengthError("Номер счёта должен состоять из 20 цифр.")
+
     return f"**{str(account_number)[-4:]}"
